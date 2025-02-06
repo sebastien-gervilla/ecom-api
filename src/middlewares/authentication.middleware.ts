98eee381754186f17a4a64e4290b8c61b4104a11
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2025-02-06 12:25:34 +0100

    feat: Auth middleware

---
+++ src/middlewares/authentication.middleware.ts
@@ -0,0 +1,23 @@
+import { environment } from '@/configuration/environment';
+import { Interfaces } from '@/interfaces';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import jwt from 'jsonwebtoken';
+
+export const authentication = (request: FastifyRequest, reply: FastifyReply, done: Function) => {
+    const token = request.cookies.token;
+
+    if (!token)
+        return reply.status(401).send({ message: 'Not authenticated' });
+
+    try {
+
+        const decoded = jwt.verify(token, environment.jwtSecret) as Interfaces.Users.JWTPayload;
+
+        // @ts-ignore
+        request.raw = decoded;
+
+        done();
+    } catch (error) {
+        return reply.status(401).send({ message: 'Not authenticated' });
+    }
+};
